10e55850d9a83feb5cf8b1674471a25c2af0d44b
Michiel van Dijk <michiel.vandijk@wur.nl>
2018-06-06 17:09:15 +0200

    Add yecc figures and updated report

--- Scripts/report_input/yecc.R
+++ Scripts/report_input/yecc.R
@@ -30,76 +30,80 @@ options("stringsAsFactors"=FALSE) # ensures that characterdata that is loaded (e
 options(digits=4)
 
 
-### PREPARE GAMS LINK
-igdx(GAMSPath)
-
-
 ### GET DATA
-# Read LPJML CC shifters
-yecc_lpjml_raw <- read_delim(file.path("P:/globiom/Projects/ILRI-FAO-LivestockFeedCCpaper/Biophys_CC_shifters/Shifter_ISIMIP_LUIDCountry_cropland_heterogeneous_LPJmL_v4.gms"), 
-                            delim = ",", col_names = T, skip = 13)
-
-# Read EPIC CC shifters
-yecc_epic_raw <- read_delim(file.path("P:/globiom/Projects/ILRI-FAO-LivestockFeedCCpaper/Biophys_CC_shifters/Shifter_ISIMIP_LUIDCountry_cropland_heterogeneous_EPIC_v4.gms"), 
-                         delim = ",", col_names = T, skip = 13)
+# yecc
+yecc <- read_csv(file.path(projectPath, "Data/ZMB/Processed/Yecc/yecc.csv"), guess_max = 20000) # add guess_max because otherwise it makes it integer
 
 # Read luid map
-luid_r <- readOGR("P:/globiom/Data/simu_luid_region_maps/LUId/LUID_CTY.shp")
+luid_p_zmb <- readRDS(file.path(projectPath, "Data/ZMB/Processed/Maps/luid_p_ZMB.rds"))
 
 
-### PROCESS DATA
-# LPJML
-yecc_lpjml_zmb <- filter(yecc_lpjml_raw, Country == "Zambia", 
-                         ANYRCP %in% c("rcp8p5", "noC8p5"),
-                         EPICOUTPUT == "YLDG", InputSys == "SS") %>%
-  gather(year, value, -ANYClimateModel:-EPICOUTPUT) %>%
-  filter(year == 2050) %>%
-  mutate(growth = (value - 1)*100)
+### PLOTS
+# Combine
+yecc_ag <-  yecc %>%
+  ungroup() %>%
+  group_by(ANYClimateModel, ANYCropModel, ANYRCP, Country, Crop, InputSys, year) %>%
+  summarize(yecc = sum(value * area, na.rm = T)/sum(area, na.rm = T)) %>%
+  filter(!is.nan(yecc), ANYRCP != "noC8p5")
 
-# EPIC
-yecc_epic_zmb <- filter(yecc_epic_raw, Country == "Zambia", 
-                        ANYRCP %in% c("rcp8p5", "noC8p5"),
-                        EPICOUTPUT == "YLDG", InputSys == "SS") %>%
-  gather(year, value, -ANYClimateModel:-EPICOUTPUT) %>%
-  filter(year == 2050) %>%
-  mutate(growth = (value - 1)*100)
+summary(yecc_ag)
+lapply(yecc_ag[sapply(yecc_ag, class) == "character"], unique)
+xtabs(~ ANYCropModel + ANYClimateModel + ANYRCP, data = yecc)
 
-df<- bind_rows(yecc_epic_zmb, yecc_lpjml_zmb)
-
-GCM1 . HadGEM2-ES
-GCM2 . IPSL-CM5A-LR
-GCM3 . GFDL-ESM2M
-GCM4 . MIROC-ESM-CHEM
-GCM5 . NorESM1-M
-GCMM . MeanGCM
-
-### GRAPHS
-ggplot(df) +
-  geom_boxplot(aes(x = Crop, y = growth)) +
-  facet_wrap(ANYCropModel~ANYRCP)
+# Plots
+yecc_ag_df <- yecc_ag %>%
+  filter(ANYRCP %in% c("rcp8p5")) %>%
+  filter(year == 2050, InputSys == "SS") %>%
+  mutate(growth = (yecc - 1)*100)
 
 
+fig_yecc_ag <- ggplot(yecc_ag_df, aes(x = Crop, y = growth, fill = Crop)) +
+  geom_boxplot() +
+  stat_boxplot(geom ='errorbar', width = 0.5) +
+  scale_y_continuous(breaks = seq(-150, 150, 25)) +
+  labs(x = "", y = "2010-2050 yield growth (%)", fill = "") +
+  theme_bw() +
+  theme() +
+  theme(panel.grid.major.x = element_blank(), panel.grid.minor = element_blank()) +
+  guides(fill = "none")
 
-### GRAPHS
+
+### MAPS
 # ZMB map
-zmb_map <- fortify(luid_r[luid_r$Field2 == "Zambia",], region = "Field1_1") %>%
+zmb_map <- fortify(luid_p_zmb, region = "Field1_1") %>%
   rename(LUId= id)
-zmb_df <- luid_r[luid_r$Field2 == "Zambia",]@data %>%
-  rename(id = Field1_1)
 
-model_df <- left_join(zmb_map, df)
+# Function to create yecc maps
+yecc_p_f <- function(crp, sys){
+  crop_df <- filter(yecc, Crop %in% crp, InputSys %in% sys, year == 2050, ANYRCP == "rcp8p5") %>%
+    group_by(LUId, Crop) %>%
+    summarize(value = mean(value, na.rm =T))
 
-model_df <- yecc_lpjml_zmb
-crp <- "Corn"
-
-yecc_p_f <- function(crp, model_df){
-  df <- filter(model_df, Crop == crp, InputSys == "SS", year == 2050, ANYRCP != "rcp8p5")
-  ggplot() +
-    geom_polygon(data = model_df, aes(x = long, y = lat, group = group, fill = value)) +
-    facet_grid(ANYClimateModel ~ ANYCropModel + ANYRCP) +
-    scale_fill_gradient(low = "yellow", high = "red")
+  df <- left_join(zmb_map, crop_df) %>%
+    na.omit() %>%
+    mutate(growth = (value - 1)*100) %>%
+    mutate(range = cut(growth, c(-Inf, -25, -5, 5, 25, Inf), labels = F),
+           range = factor(range, levels = c(1:5), 
+                          labels = c("yield loss > 25", "yield loss 5-25%", "yield change within 5%",
+                                     "yield gain 5-25%", "yield gain >25%")))
   
+  cols <- c("#CA0020", "#F4A582", "#F7F7F7", "#92C5DE", "#0571B0")
+  names(cols) <- c("yield loss > 25", "yield loss 5-25%", "yield change within 5%",
+  "yield gain 5-25%", "yield gain >25%")
+  
+  p = ggplot() +
+    geom_polygon(data = luid_p_zmb, aes (x = long, y = lat, group = group), colour = "black", fill = "grey50") +
+    geom_polygon(data = df, aes(x = long, y = lat, group = group, fill = range)) +
+    geom_path(data = luid_p_zmb, aes (x = long, y = lat, group = group), colour = "black") +
+    labs(fill = "") +
+    scale_fill_manual(values = cols) +
+    coord_map() +
+    theme_void() +
+    facet_wrap(~Crop, nrow = 2) +
+    theme(strip.text = element_text(face = "bold"))
+  p
+
 }
 
+fig_yecc_crop <- yecc_p_f(c("Corn", "Cass", "Gnut", "Soya"), "SS")
 
-### PLOT MAPS
