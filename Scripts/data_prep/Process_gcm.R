abd79ff32203c5920f00737b7eb864dbe4c1ff01
michielvandijk <michiel.vandijk@wur.nl>
2018-10-18 11:54:58 +0200

    add

--- Scripts/data_prep/Process_gcm.R
+++ Scripts/data_prep/Process_gcm.R
@@ -35,7 +35,8 @@ luid_p_zmb <- readRDS(file.path(projectPath, "Data/ZMB/Processed/Maps/luid_p_ZMB
 zmb_map <- fortify(luid_p_zmb, region = "Field1_1") %>%
   rename(LUId= id)
 
-# http://neondataskills.org/R/Multi-Band-Rasters-In-R/
+# fig_aez
+aez <- readOGR(file.path(projectPath, "/Data/ZMB/Raw/Spatial_data/aez/aez.shp"))
 
 # Set ISIMIP path
 isimip_path <- "P:/watxene/Wat-Data/ISI-MIP1/multi-GCM_input"
@@ -150,8 +151,8 @@ tic()
 cc_tas <- list()
 
 # gfdl_esm2m
-cc_tas[[1]] <- process_cc_f("gfdl-esm2m", 1981, 2000, "tas", "hist/tas", luid_p_zmb)
-cc_tas[[2]] <- process_cc_f("gfdl-esm2m", 2041, 2060, "tas", "rcp8p5/tas", luid_p_zmb)
+cc_tas[[1]] <- process_cc_f("gfdl-esm2m", 1981, 2000, "tas", "hist/tas", aez)
+cc_tas[[2]] <- process_cc_f("gfdl-esm2m", 2041, 2060, "tas", "rcp8p5/tas", aez)
 
 # hadhem2-es
 cc_tas[[3]] <- process_cc_f("hadgem2-es", 1981, 2000, "tas", "hist/tas", luid_p_zmb)
@@ -194,89 +195,41 @@ cc_pr[[9]] <- process_cc2_f("noresm1-m", 1981, 2000, "pr", "hist/pr", luid_p_zmb
 cc_pr[[10]] <- process_cc2_f("noresm1-m", 2041, 2060, "pr", "rcp8p5/pr", luid_p_zmb)
 
 
+### PLOT TAS
+# smooth and stack
+tas <- stack(lapply(cc_tas, function(x) disaggregate(x, 6, method='bilinear')))
+plot(tas)
 
-# Plot difference between base year and projection
+# convert to df and calculate difference
+tas_df <- as.data.frame(rasterToPoints(tas)) %>%
+  gather(file, value, -x, -y) %>%
+  separate(file, c("gcm", "type", "variable"), sep = "_") %>%
+  spread(type, value) %>%
+  mutate(value = rcp8p5 - hist,
+         bin = cut(value, seq(1.9, 2.5, 0.1), labels = c(seq(2, 2.5, 0.1))))
+  
 
-y <- disaggregate(tas_1980, 6, method='bilinear') # to 5 arcmin
-plot(y)
-m <- mask(y, luid_p_zmb)
-plot(m)
-plot(luid_p_zmb, add = T)
-plot(tas_1980)
-test <- raster2df_f("gfdl-esm2m", 1980, 1981, "tas", "hist", luid_p_zmb)
+# Prepare AEZ
+aez_df <- aez@data %>%
+  mutate(id = rownames(.))
 
-ggplot() +
-  geom_raster(data = test, aes(x = x, y = y, fill = value)) +
+aez_for <- fortify(aez) %>%
+  left_join(aez_df)
+
+# Plot
+# Set number of colors
+n_col <- length(unique(tas_df$bin))
+col <- c(brewer.pal(5,"Reds"))
+
+p = ggplot() +
+  geom_raster(data = tas_df, aes(x = x, y = y, fill = bin)) +
+  geom_path(data = aez_for, aes(x = long, y = lat, group = group)) +
   coord_cartesian() +
-  theme_void()
+  facet_wrap(~gcm) +
+  scale_fill_manual(values = brewer.pal(n = n_col, name = "Reds")) +
+  labs(x="", y="", fill = "Absolute change (K)") +
+  theme_void() +
+  theme(legend.position = "bottom", legend.title.align=0.5) +
+  guides(fill = guide_legend(label.position = "bottom", title.position = "top", nrow = 1, colour = "black")) 
 
-### EU28 PLOT OF ABSOLUTE VALUES
-eu28 <- iso3c2region %>%
-  filter(region2 == "EU28")
-yld_eu28 <- filter(yld_eu28, iso3c %in% eu28$iso3c)
-
-sum_yld_eu28 <- yld_eu28 %>%
-  group_by(crop, system, clim_scen, iso3c) %>%
-  summarize(mean = mean(value, na.rm = T),
-            min = min(value, na.rm = T),
-            max = max(value, na.rm = T))
-
-# Plots
-yld_eu28_noirr_mai <- filter(yld_eu28, system == "noirr", crop == "mai") 
-
-ggplot(data = yld_eu28_noirr_mai, aes(x = clim_scen, y = value), outlier.colour = NA) +
-  #geom_boxplot() +
-  geom_violin(draw_quantiles = c(0.25, 0.5, 0.75), aes(fill = clim_scen)) +
-  labs(title = paste(unique(yld_eu28_noirr_mai$system), unique(yld_eu28_noirr_mai$crop), sep = "-")) +
-  #geom_jitter(height = 0, width = 0.1) +
-  facet_wrap(~iso3c, scales = "free") +
-  theme(legend.position = "bottom")
-
-
-### SSA PLOT OF ABSOLUTE VALUES
-ssa <- iso3c2region %>%
-  filter(region2 == "SSA")
-yld_ssa <- filter(yld_ssa, iso3c %in% ssa$iso3c)
-
-sum_yld_ssa <- yld_ssa %>%
-  group_by(crop, system, clim_scen, iso3c) %>%
-  summarize(mean = mean(value, na.rm = T),
-            min = min(value, na.rm = T),
-            max = max(value, na.rm = T))
-
-# Plots
-yld_ssa_noirr_mai <- filter(yld_ssa, system == "noirr", crop == "mai") 
-
-ggplot(data = yld_ssa_noirr_mai, aes(x = clim_scen, y = value), outlier.colour = NA) +
-  #geom_boxplot() +
-  labs(title = paste(unique(yld_ssa_noirr_mai$system), unique(yld_ssa_noirr_mai$crop), sep = "-")) +
-  geom_violin(draw_quantiles = c(0.25, 0.5, 0.75), aes(fill = clim_scen)) + 
-  facet_wrap(~ iso3c, scales = "free")
-
-
-### EU PLOT OF GROWTH
-# Calculate average historical yld per grid cell
-yld_eu28_av <- yld_eu28 %>%
-  filter(clim_scen == "all-hist") %>%
-  group_by(x, y, iso3c, crop, system, clim_scen) %>%
-  summarize(average = mean(value, na.rm = T)) %>%
-  dplyr::select(-clim_scen)
-
-# Calculate change relative to all-hist average
-yld_eu28_rel <- left_join(yld_eu28, yld_eu28_av) %>%
-  mutate(value = value/average) %>%
-  filter(!is.na(value))
-summary(yld_eu28_rel)
-
-
-# Plots
-yld_eu28_noirr_mai_rel <- filter(yld_eu28_rel, system == "noirr", crop == "mai") 
-
-ggplot(data = yld_eu28_noirr_mai_rel, aes(x = clim_scen, y = value), outlier.colour = NA) +
-  #geom_boxplot() +
-  geom_violin(draw_quantiles = c(0.25, 0.5, 0.75), aes(fill = clim_scen)) + 
-  labs(title = paste(unique(yld_eu28_noirr_mai_rel$system), unique(yld_eu28_noirr_mai_rel$crop), sep = "-")) +
-  #geom_jitter(height = 0, width = 0.1) +
-  facet_wrap(~iso3c, scales = "free") +
-  theme(legend.position = "bottom")
-
+p
